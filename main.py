69aab9d9be7bb4470ebe01b65f2293ba62f4704e
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-11-14 20:51:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,48 @@ from sqlalchemy import Integer, String, Boolean
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+bootstrap = Bootstrap5(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/cafes")
+def all_cafes():
+    return render_template("cafes.html")
+
+
+@app.route("/delete/<int:cafe_id>")
+def delete_cafe(cafe_id):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
